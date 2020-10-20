d9edc6564f92b42a9e81e66749ac94a8f161c8c5
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-20 13:04:05 -0600

    Simplify useGithubJobsApi hook to return all jobs

--- src/useGithubJobsApi.ts
+++ src/useGithubJobsApi.ts
@@ -1,32 +1,23 @@
 import { useState, useEffect } from "react"
 
-export function useGithubJobsApi(): any[] {
-  const [allJobs, setAllJobs] = useState<any[]>()
-  const [viewedJobs, setViewedJobs] = useState<any[]>([])
-  const viewedJobsCountLimit = 12
+export function useGithubJobsApi() {
+  const [jobs, setJobs] = useState<any[]>()
 
   useEffect(() => {
     async function fetchData() {
       const request = await fetch(
         "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json"
       )
-      console.log("fetching...")
+      // console.log("fetching...")
       const response = await request.json()
       const data = response
-      console.log("data fetched!")
-      if (!allJobs) {
-        setAllJobs(data)
+      // console.log("data fetched!")
+      if (!jobs) {
+        setJobs(data)
       }
     }
     fetchData()
   })
 
-  useEffect(() => {
-    if (allJobs) {
-      const firstTwelveJobs: any[] = allJobs.slice(0, viewedJobsCountLimit)
-      setViewedJobs(firstTwelveJobs)
-    }
-  }, [allJobs])
-
-  return viewedJobs
+  return jobs
 }
