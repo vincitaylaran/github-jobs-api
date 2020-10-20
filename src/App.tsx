b329773f439bdd1483873581ec0be2ad625d07d5
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-20 10:11:26 -0600

    include file for TSLint and Prettier configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
+// TODO: fix Prettier extension
+
+export default function App() {
+  const [allJobs, setAllJobs] = React.useState<any[]>()
+  const [viewedJobs, setViewedJobs] = React.useState<any[]>()
+  const viewedJobsCountLimit = 12
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const request = await fetch(
+        "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json"
+      )
+      console.log("fetching...")
+      const response = await request.json()
+      const data = response
+      console.log("data -> ", data)
+      if (!allJobs) {
+        setAllJobs(data)
+      }
+    }
+    fetchData()
+  })
+
+  React.useEffect(() => {
+    if (allJobs) {
+      const firstTwelveJobs: any[] = allJobs.slice(0, viewedJobsCountLimit)
+      setViewedJobs(firstTwelveJobs)
+    }
+  }, [allJobs])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Jobs</h1>
+      <h2>Showing "{viewedJobs && viewedJobs.length}" jobs</h2>
+      <h2>There are "{allJobs && allJobs.length}" jobs in total</h2>
+      {allJobs && viewedJobs ? (
+        viewedJobs.map(job => <p>{job.company}</p>)
+      ) : (
+        <h3>Loading...</h3>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
