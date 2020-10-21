6c30e5d24c66ffac8421d2441283bc254e13ad78
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-21 17:04:23 -0600

    Update useGithubJobsApi to fetch jobs in increments of 50 upon first load and upon clicking load more button

--- src/useGithubJobsApi.ts
+++ src/useGithubJobsApi.ts
@@ -1,23 +1,24 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 
-export function useGithubJobsApi() {
-  const [jobs, setJobs] = useState<any[]>()
+export function useGithubJobsApi(pageNumber: number) {
+  const [jobs, setJobs] = useState<any[]>([])
+
+  const fetchData = useCallback(async () => {
+    const request = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=${pageNumber}`
+    )
+    const response = await request.json()
+    const data = response
+
+    if (data.length > 0) {
+      setJobs(jobs.concat(data))
+      console.log(data)
+    }
+  }, [pageNumber])
 
   useEffect(() => {
-    async function fetchData() {
-      const request = await fetch(
-        "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json"
-      )
-      // console.log("fetching...")
-      const response = await request.json()
-      const data = response
-      // console.log("data fetched!")
-      if (!jobs) {
-        setJobs(data)
-      }
-    }
     fetchData()
-  })
+  }, [fetchData])
 
   return jobs
 }
