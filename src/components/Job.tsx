992ba98ee24d8e3ac9171aeceb996afdbdc67dbb
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-23 11:05:31 -0600

    Create component for job

---
+++ src/components/Job.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+interface Props {
+  company: string
+  company_logo: string
+  company_url: string
+  created_at: string
+  description: string
+  how_to_apply: string
+  id: string
+  location: string
+  title: string
+  type: string
+  url: string
+}
+
+export const Job: React.FC<Props> = ({ ...job }) => {
+  console.log(job)
+
+  const { id, company, type, location } = job
+
+  return (
+    <p
+      key={id}
+      style={{ border: "1px solid", cursor: "pointer" }}
+      onClick={() => console.log("hello")}
+    >
+      {company} <br />
+      type: {type} <br />
+      location: {location}
+    </p>
+  )
+}
