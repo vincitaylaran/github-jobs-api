d7bad4cd9c279befbf6c8345eb95ad246a3714ab
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-23 11:06:22 -0600

    Import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,26 @@
-import React, { useState } from "react"
+import React from "react"
 import "./App.css"
 import { useGithubJobsApi } from "./useGithubJobsApi"
+import { Filters } from "./components/Filters"
+import { LoadButton } from "./components/LoadButton"
+import { Jobs } from "./components/Jobs"
+import { Job } from "./components/Job"
 
 export default function App() {
-  const [description, setDescription] = useState<string>()
-  const [location, setLocation] = useState<string>()
-  const [isFullTimeOnly, setIsFullTimeOnly] = useState<boolean>()
-  const { jobs, fetchData, loadMore } = useGithubJobsApi()
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault()
-    fetchData({
-      description,
-      location,
-      isFullTimeOnly,
-      pageNumber: 1,
-    })
-  }
+  const { jobs, fetchData, loadMore, isLoading } = useGithubJobsApi()
 
   return (
     <div className="App">
       <h1>GitHub Jobs</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Filter by title, companies, expertise..."
-          style={{ width: 250 }}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="Filter by location..."
-          style={{ width: 250 }}
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <br />
-        Full Time Only
-        <input
-          type="checkbox"
-          checked={isFullTimeOnly}
-          onClick={() => setIsFullTimeOnly(isFullTimeOnly ? false : true)}
-        />
-        <br />
-        <button>Search</button>
-      </form>
-      <button onClick={loadMore}>Load more</button>
-
+      <Filters onSearch={fetchData} />
+      <LoadButton onClick={loadMore}>Load more</LoadButton>
       <h2>Showing "{jobs && jobs.length}" jobs</h2>
-      {jobs ? (
-        jobs.map((job, index) => (
-          <p
-            key={job.id}
-            style={{ border: "1px solid", cursor: "pointer" }}
-            onClick={() => console.log("hello")}
-          >
-            {index}: {job.company} <br />
-            type: {job.type} <br />
-            location: {job.location}
-          </p>
-        ))
-      ) : (
-        <h3>Loading...</h3>
-      )}
-      <button onClick={loadMore}>Load more</button>
+      <Jobs>
+        {jobs.map((job) => (
+          <Job {...job} />
+        ))}
+      </Jobs>
+      <LoadButton onClick={loadMore}>Load more</LoadButton>
     </div>
   )
 }
