3a29287439127230e5aad5f12feb9499b4620011
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-26 08:00:40 -0600

    Create a path to job description

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "./App.css"
 import { Home } from "./pages/Home"
+import { JobDescription } from "./pages/JobDescription"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
@@ -8,8 +9,11 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/job/:id">
+          <JobDescription />
         </Route>
       </Switch>
     </Router>
