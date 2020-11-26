a25d70cec57cc8a6c6b1400184111b83beb788e5
Vinci Taylaran <svl.taylaran@gmail.com>
2020-11-26 10:59:59 -0700

    Create Filters.test.tsx

---
+++ src/components/__tests__/Filters.test.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { shallow } from "enzyme"
+import { Filters } from "../Filters"
+import { FilterButton } from "../FilterButton"
+
+describe("<Filters />", () => {
+  it("shows 3 input elements", () => {
+    const wrapper = shallow(
+      <Filters onSearch={() => console.log("Filter Button clicked")} />
+    )
+    expect(wrapper.find("input").length).toEqual(3)
+  })
+
+  it("shows the Filter Button component", () => {
+    const wrapper = shallow(
+      <Filters onSearch={() => console.log("Filter Button clicked")} />
+    )
+    expect(wrapper.find(FilterButton).length).toEqual(1)
+  })
+})
