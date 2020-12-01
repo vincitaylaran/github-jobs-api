08b94d83b07e418702c1e527f92169976f18d69a
Vinci Taylaran <svl.taylaran@gmail.com>
2020-12-01 12:54:02 -0700

    Move App.tsx to components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { createContext } from "react"
-import "App.css"
+import "styles/App.css"
 import { Home } from "pages/Home"
 import { JobDescription } from "pages/JobDescription"
 
@@ -9,23 +9,19 @@ import { useDarkTheme } from "hooks/useDarkTheme"
 
 import { ThemeButton } from "components/ThemeButton"
 
-export const ThemeContext = createContext("light")
-
 export default function App() {
-  const { theme, toggleTheme } = useDarkTheme()
+  const { toggleTheme } = useDarkTheme()
 
   return (
     <Router>
       <ThemeButton onClick={toggleTheme} />
       <Switch>
-        <ThemeContext.Provider value={theme}>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/job/:id">
-            <JobDescription />
-          </Route>
-        </ThemeContext.Provider>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/job/:id">
+          <JobDescription />
+        </Route>
       </Switch>
     </Router>
   )
