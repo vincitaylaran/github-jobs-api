391a6c9e2292f3a0c640076e0f479466b4705671
Vinci Taylaran <svl.taylaran@gmail.com>
2020-12-01 12:54:49 -0700

    Add beforeEach, remove describe

--- src/pages/__tests__/Home.test.tsx
+++ src/pages/__tests__/Home.test.tsx
@@ -6,19 +6,20 @@ import { Jobs } from "components/Jobs"
 import { Filters } from "components/Filters"
 import { LoadButton } from "components/LoadButton"
 
-describe("<Home />", () => {
-  it("shows Filters", () => {
-    const wrapper = shallow(<Home />)
-    expect(wrapper.find(Filters).length).toEqual(1)
-  })
+let component: any
 
-  it("shows Jobs", () => {
-    const wrapped = shallow(<Home />)
-    expect(wrapped.find(Jobs).length).toEqual(1)
-  })
+beforeEach(() => {
+  component = shallow(<Home />)
+})
 
-  it("shows Load Button", () => {
-    const wrapped = shallow(<Home />)
-    expect(wrapped.find(LoadButton).length).toEqual(1)
-  })
+it("shows Filters", () => {
+  expect(component.find(Filters).length).toEqual(1)
+})
+
+it("shows Jobs", () => {
+  expect(component.find(Jobs).length).toEqual(1)
+})
+
+it("shows Load Button", () => {
+  expect(component.find(LoadButton).length).toEqual(1)
 })
