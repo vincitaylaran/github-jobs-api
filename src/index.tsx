f9fe2b7bb66cb41767d76d158c39a4c59b21f27b
Vinci Taylaran <svl.taylaran@gmail.com>
2020-12-10 11:44:59 -0700

    Wrap App with Root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "components/App"
+import Root from "Root"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root>
+      <App />
+    </Root>
   </React.StrictMode>,
   document.getElementById("root")
 )
