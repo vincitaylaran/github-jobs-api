12f0ae3e389035342657ec907c529c974790b583
Vinci Taylaran <svl.taylaran@gmail.com>
2020-12-21 17:58:55 -0700

    Create styles for Job component, apply styles to App

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
 import "styles/Job.scss"
+import dayjs from "dayjs"
 
 export interface Props {
   company: string
-  company_logo: string
-  company_url: string
+  company_logo: string | null
+  company_url: string | null
   created_at: string
   description: string
   how_to_apply: string
@@ -20,6 +21,41 @@ export const Job: React.FC<Props> = ({ ...job }) => {
   const { id, company, type, location, created_at, title } = job
   const history = useHistory()
 
+  function getDifference(createdAt: string): string {
+    const firstDate = dayjs(createdAt)
+    const currentDate = dayjs()
+    const differenceInMinutes = currentDate.diff(firstDate, "minute")
+    const minutesInDay = 1440
+    const minutesInWeek = 10080
+    const minutesInMonth = 43800
+    const minutesInYear = 525600
+
+    if (differenceInMinutes < 60) {
+      return `${differenceInMinutes}m`
+    } else if (differenceInMinutes > 60 && differenceInMinutes < minutesInDay) {
+      return `${currentDate.diff(firstDate, "hour")}h`
+    } else if (
+      differenceInMinutes > minutesInDay &&
+      differenceInMinutes < minutesInWeek
+    ) {
+      return `${currentDate.diff(firstDate, "day")}d`
+    } else if (
+      differenceInMinutes > minutesInWeek &&
+      differenceInMinutes < minutesInMonth
+    ) {
+      return `${currentDate.diff(firstDate, "week")}w`
+    } else if (
+      differenceInMinutes > minutesInMonth &&
+      differenceInMinutes < minutesInYear
+    ) {
+      return `${currentDate.diff(firstDate, "month")}m`
+    } else if (differenceInMinutes >= minutesInYear) {
+      return `${currentDate.diff(firstDate, "year")}y`
+    } else {
+      return `${currentDate.diff(firstDate, "day")}d`
+    }
+  }
+
   return (
     <div
       key={id}
@@ -28,14 +64,21 @@ export const Job: React.FC<Props> = ({ ...job }) => {
         history.push(`/job/${id}`)
       }}
     >
-      <div>IMAGE HERE</div>
-      <div style={{ backgroundColor: "violet" }}>
-        <span>{created_at}</span>
-        <span>{type}</span>
+      <span className="companyLogo ">{company[0]}</span>
+      <div className="jobBody">
+        <div className="jobDurationAndType">
+          <span>{getDifference(created_at)} ago</span>
+          <span style={{ margin: "0 10px" }}>•</span>
+          <span>{type}</span>
+        </div>
+        <div className="jobTitle">{title}</div>
+        <div className="jobCompany">{company}</div>
+        {/* <div className="jobLocation padding-left-20">{location}</div> */}
+        <div className="jobLocation">
+          {location}
+          {/* Columbus, OH */}
+        </div>
       </div>
-      <div style={{ backgroundColor: "brown" }}>{title}</div>
-      <div style={{ backgroundColor: "orange" }}>{company}</div>
-      <div style={{ backgroundColor: "salmon" }}>{location}</div>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "components/App"
 import Root from "Root"
+import "styles/App.scss"
 
 ReactDOM.render(
   <React.StrictMode>
     <Root>
-      <App />
+      <div className="app">
+        <App />
+      </div>
     </Root>
   </React.StrictMode>,
   document.getElementById("root")
