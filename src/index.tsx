23828960994d34b9746499ac64940f4b28d15306
Vinci Taylaran <svl.taylaran@gmail.com>
2021-01-12 10:36:53 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "components/App"
 import Root from "Root"
-import "styles/App.scss"
 
 ReactDOM.render(
   <React.StrictMode>
